20defb7093c4a26f10acc120d014819b252fc53e
gomaa elmokhtar <gomaamok44@gmail.com>
2019-12-15 22:40:11 +0200

    Updates

---
+++ precache-manifest.2092a6a999c2b9d0b666929858b506cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd6d81a9fb3ebcf9a2c3b7852dcf7c7",
+    "url": "/REMINDER-APP/index.html"
+  },
+  {
+    "revision": "624dca48792308eb9e06",
+    "url": "/REMINDER-APP/static/css/2.821458bd.chunk.css"
+  },
+  {
+    "revision": "a9e30636ef4e161f7a71",
+    "url": "/REMINDER-APP/static/css/main.e6d481d3.chunk.css"
+  },
+  {
+    "revision": "624dca48792308eb9e06",
+    "url": "/REMINDER-APP/static/js/2.5b776772.chunk.js"
+  },
+  {
+    "revision": "0d51a26e357591260d29df3c0b1559d1",
+    "url": "/REMINDER-APP/static/js/2.5b776772.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9e30636ef4e161f7a71",
+    "url": "/REMINDER-APP/static/js/main.8fbf102f.chunk.js"
+  },
+  {
+    "revision": "efba6ad949c8dc8fb7f3",
+    "url": "/REMINDER-APP/static/js/runtime-main.519990a0.js"
+  },
+  {
+    "revision": "9bd2dd25cd53021753942e8d3ab95c48",
+    "url": "/REMINDER-APP/static/media/logo.9bd2dd25.png"
+  }
+]);
